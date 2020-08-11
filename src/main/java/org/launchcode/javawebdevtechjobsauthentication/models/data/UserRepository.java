5c3d92cab446e1593edf8c503317a1307bc23d4a
Kenan Kadic <ken.kadic@outlook.com>
2020-08-10 20:17:09 -0500

    initial commit

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/UserRepository.java
@@ -4,8 +4,6 @@ import org.launchcode.javawebdevtechjobsauthentication.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
